bcc9467a655361ca9d1745fbbece290f60ca6f99
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-06-30 22:22:04 +0530

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Towards Ikhlaas",
-  description: "Towards Ikhlaas Foundation landing page. Under construction.",
+  title: "SimplyQibla - Accurate Qibla",
+  description: "SimplyQibla landing page. Get the app on Play or App Store.",
 };
 
 export default function RootLayout({ children }) {
